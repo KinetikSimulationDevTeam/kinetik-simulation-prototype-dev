3133b27dedeb4dafaf4ca110cdbc879beaf13b2d
Tao Huang <thuang@unc.edu>
2023-05-01 14:23:07 -0400

    Add Jest tests

--- src/Tests/.test.js
+++ src/Tests/.test.js
@@ -1,38 +1,30 @@
 import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 import DragDropFile from "../Components/DragDropFile";
+import NavigationBar from "../Components/NavigationBar";
+import Sliders from "../Components/Sliders";
+import NewOpsModule from "../Modules/NewOpsModule";
 
 describe("DragDropFile", () => {
-  test("should render DragDropFile component", () => {
-    const { container } = render(<DragDropFile />);
-    expect(container).toMatchSnapshot();
-  });
+    test("should render DragDropFile component", () => {
+        render(<DragDropFile />);
+    });
+});
 
-  test("should show selected file name when file is selected with click", async () => {
-    const onActionMock = jest.fn();
-    const { getByTestId } = render(<DragDropFile onAction={onActionMock} />);
-    const file = new File(["file contents"], "test.csv", { type: 'text/csv' });
-    const input = getByTestId("input-file");
+describe("NavigationBar", () => {
+    test("should render NavigationBar component", () => {
+        render(<NavigationBar />);
+    });
+});
 
-    fireEvent.change(input, { target: { files: [file] } });
-    expect(screen.getByText(`Selected file - ${file.name}`)).toBeInTheDocument();
-    expect(onActionMock).toHaveBeenCalledWith(file.name);
-  });
+describe("Sliders", () => {
+    test("should render Sliders component", () => {
+        render(<Sliders />);
+    });
+});
 
-  test("should show error message when no file is selected", async () => {
-    const { getByTestId } = render(<DragDropFile />);
-    const submitButton = getByTestId("submit-button");
-
-    fireEvent.click(submitButton);
-    expect(screen.getByText("Please select a file")).toBeInTheDocument();
-  });
-
-  test("should show success message when file is already uploaded", async () => {
-    localStorage.setItem("KinetikDataSet", "test");
-    const { getByTestId } = render(<DragDropFile />);
-    const submitButton = getByTestId("submit-button");
-
-    fireEvent.click(submitButton);
-    expect(screen.getByText("File already uploaded")).toBeInTheDocument();
-  });
+describe("NewOpsModule", () => {
+    test("should render NewOpsModule component", () => {
+        render(<NewOpsModule />);
+    });
 });
