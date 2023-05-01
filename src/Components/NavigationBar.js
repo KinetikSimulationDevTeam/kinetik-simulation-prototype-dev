92d799a56fc973a2a4869ff74a35acc1dc8ee194
Tao Huang <thuang@unc.edu>
2023-05-01 15:14:28 -0400

    Add Alert in NavBar

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Kinetik_img from '../Images/Kinetik_logo.png';
+import alertify from 'alertifyjs';
 
 const Navbar = () => {
+    const helpFunction = () => {
+        alertify.alert('User Manual', 'For User Manual, Please visit: https://tarheels.live/comp523s23teaml/d4-documentation/');
+    }
+
     return (
         <nav id='nav-bar'>
             <a href="https://www.kinetiksimulation.com" target='_blank'><img src={Kinetik_img} alt='' width={30} height={30} /></a>
@@ -9,7 +14,7 @@ const Navbar = () => {
                 <a id='home-link' className='link' href=""> Home<span>(current)</span></a>
             </li>
             <li>
-                <a className='link' href="#"> Help </a>
+                <a className='link' href="#" onClick={helpFunction}> Help </a>
             </li>
             <li>
                 <a className='link' href="https://www.kinetiksimulation.com" target='_blank'> About </a>
