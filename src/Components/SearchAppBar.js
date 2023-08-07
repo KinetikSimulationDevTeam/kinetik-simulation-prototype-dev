79cee794b51a17315b02ae2d04060a07dcc826c8
Tao Huang <thuang@unc.edu>
2023-08-07 15:02:34 -0400

    Adjust upload files link in search bar

--- src/Components/SearchAppBar.js
+++ src/Components/SearchAppBar.js
@@ -1,38 +1,68 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import Logo from '../Images/Logo.png'
-import { Link } from 'react-router-dom';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Autocomplete from '@mui/material/Autocomplete';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import Logo from "../Images/Logo.png";
+import { Link } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Autocomplete from "@mui/material/Autocomplete";
 import InputAdornment from "@mui/material/InputAdornment";
 
 const searchResult = [
-  { tab: 'Go to: Slider UX', group: 'Forecast', link: '/', value: 'slider ux' },
-  { tab: 'Go to: Upload file', group: 'Forecast', link: '/', value: 'upload file' },
-  { tab: 'Go to: Statistics session', group: 'Forecast', link: '/', value: 'statistics' },
-  { tab: 'Go to: FAQ', group: 'Help', link: '/help', value: 'faq' },
-  { tab: 'Go to: Sign in', group: 'Sign in', link: '/signin', value: 'sign in' },
-  { tab: 'Go to: Sign up', group: 'Sign in', link: '/signin', value: 'sign up' },
-  { tab: 'Go to: Forgot password', group: 'Sign in', link: '/signin', value: 'forgot password' },
-  { tab: 'Outbound link to: About Kinetik', group: 'Sign in', link: '/', value: 'about kinetik' },
-]
+  { tab: "Go to: Slider UX", group: "Forecast", link: "/", value: "slider ux" },
+  {
+    tab: "Go to: Upload file",
+    group: "Upload Files",
+    link: "/files",
+    value: "upload file",
+  },
+  {
+    tab: "Go to: Statistics session",
+    group: "Forecast",
+    link: "/",
+    value: "statistics",
+  },
+  { tab: "Go to: FAQ", group: "Help", link: "/help", value: "faq" },
+  {
+    tab: "Go to: Sign in",
+    group: "Sign in",
+    link: "/signin",
+    value: "sign in",
+  },
+  {
+    tab: "Go to: Sign up",
+    group: "Sign in",
+    link: "/signin",
+    value: "sign up",
+  },
+  {
+    tab: "Go to: Forgot password",
+    group: "Sign in",
+    link: "/signin",
+    value: "forgot password",
+  },
+  {
+    tab: "Outbound link to: About Kinetik",
+    group: "Sign in",
+    link: "/",
+    value: "about kinetik",
+  },
+];
 
 export default function PrimarySearchAppBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-  const [searchValue, setSearchValue] = React.useState('');
+  const [searchValue, setSearchValue] = React.useState("");
 
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
@@ -52,71 +82,73 @@ export default function PrimarySearchAppBar() {
 
   const handleSearchSubmit = (event) => {
     event.preventDefault();
-    if(searchValue.length > 0) {
-      if(searchResult.find((result) => (result.tab).toLowerCase() === searchValue)) {
-        if(searchValue === 'outbound link to: about kinetik') {
-          window.open('https://www.kinetiksimulation.com', '_blank');
-        }else {
-          window.location.href = searchResult.find((result) => (result.tab).toLowerCase() === searchValue).link;
+    if (searchValue.length > 0) {
+      if (
+        searchResult.find((result) => result.tab.toLowerCase() === searchValue)
+      ) {
+        if (searchValue === "outbound link to: about kinetik") {
+          window.open("https://www.kinetiksimulation.com", "_blank");
+        } else {
+          window.location.href = searchResult.find(
+            (result) => result.tab.toLowerCase() === searchValue
+          ).link;
         }
-      }else if(searchResult.find((result) => result.value === searchValue)) {
-        if(searchValue === 'about kinetik') {
-          window.open('https://www.kinetiksimulation.com', '_blank');
-        }else {
-          window.location.href = searchResult.find((result) => result.value === searchValue).link;
+      } else if (searchResult.find((result) => result.value === searchValue)) {
+        if (searchValue === "about kinetik") {
+          window.open("https://www.kinetiksimulation.com", "_blank");
+        } else {
+          window.location.href = searchResult.find(
+            (result) => result.value === searchValue
+          ).link;
         }
       }
     }
-  }
+  };
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={menuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
       <MenuItem onClick={handleMenuClose}>
-        <Link to={'/signin'} style={{textDecoration: 'none', color: 'white'}}>
+        <Link to={"/signin"} style={{ textDecoration: "none", color: "white" }}>
           My account
         </Link>
       </MenuItem>
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={mobileMenuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
       <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="notifications"
-          color="inherit"
-        >
+        <IconButton size="large" aria-label="notifications" color="inherit">
           <Badge badgeContent={0} color="error">
             <NotificationsIcon />
           </Badge>
@@ -124,7 +156,7 @@ export default function PrimarySearchAppBar() {
         <p>Notifications</p>
       </MenuItem>
       <MenuItem>
-        <Link to={'/signin'} style={{textDecoration: 'none', color: 'white'}}>
+        <Link to={"/signin"} style={{ textDecoration: "none", color: "white" }}>
           <IconButton
             size="large"
             aria-label="account of current user"
@@ -148,17 +180,13 @@ export default function PrimarySearchAppBar() {
             variant="h6"
             noWrap
             component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
+            sx={{ display: { xs: "none", sm: "block" } }}
           >
-            <a href="https://www.kinetiksimulation.com" target='_blank'>
-              <img src={Logo} alt='logo' id='kinetik-logo'/>
+            <a href="https://www.kinetiksimulation.com" target="_blank">
+              <img src={Logo} alt="logo" id="kinetik-logo" />
             </a>
           </Typography>
-          <Stack
-            sx={{width: '50%',
-              margin: 'auto',
-            }}
-          >
+          <Stack sx={{ width: "50%", margin: "auto" }}>
             <Autocomplete
               id="app-features"
               freeSolo
@@ -167,8 +195,8 @@ export default function PrimarySearchAppBar() {
               sx={{
                 "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
                   border: "1px solid rgb(27, 150, 255)",
-                  borderRadius: '5px'
-                }
+                  borderRadius: "5px",
+                },
               }}
               renderInput={(params) => (
                 <TextField
@@ -176,38 +204,42 @@ export default function PrimarySearchAppBar() {
                   label="Search..."
                   InputProps={{
                     ...params.InputProps,
-                    type: 'search',
+                    type: "search",
                     endAdornment: (
                       <InputAdornment position="end">
-                        <SearchIcon 
-                          style={{ color: 'rgb(27, 150, 255)', cursor: 'pointer' }}
+                        <SearchIcon
+                          style={{
+                            color: "rgb(27, 150, 255)",
+                            cursor: "pointer",
+                          }}
                           onClick={handleSearchSubmit}
                         />
                       </InputAdornment>
-                    )
+                    ),
                   }}
                   size="small"
-                  onChange={(event) => setSearchValue((event.target.value).toLowerCase())}
-                  sx={{ input: { color: 'white' } }}
+                  onChange={(event) =>
+                    setSearchValue(event.target.value.toLowerCase())
+                  }
+                  sx={{ input: { color: "white" } }}
                   InputLabelProps={{
-                    style: { color: 'rgb(27, 150, 255)' },
+                    style: { color: "rgb(27, 150, 255)" },
                   }}
                 />
               )}
             />
           </Stack>
           <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton
-              size="large"
-              aria-label="notifications"
-              color="inherit"
-            >
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton size="large" aria-label="notifications" color="inherit">
               <Badge badgeContent={0} color="error">
                 <NotificationsIcon />
               </Badge>
             </IconButton>
-            <Link to={'/signin'} style={{textDecoration: 'none', color: 'white'}}>
+            <Link
+              to={"/signin"}
+              style={{ textDecoration: "none", color: "white" }}
+            >
               <IconButton
                 size="large"
                 edge="end"
@@ -220,7 +252,7 @@ export default function PrimarySearchAppBar() {
               </IconButton>
             </Link>
           </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="show more"
