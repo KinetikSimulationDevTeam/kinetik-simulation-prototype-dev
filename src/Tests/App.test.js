6c8450cdcdff99427852cfa1f5f687748ba97100
Tao Huang <thuang@unc.edu>
2023-08-09 15:34:56 -0400

    Remove frontend test

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -1,14 +1,3 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import DragDropFile from "../Components/ControlPanelComponents/FileUpload";
-
-describe("Render Learn React App", () => {
-  let onActionMock;
-  beforeEach(() => {
-    onActionMock = jest.fn();
-  });
-
-  it("renders without crashing", () => {
-    render(<DragDropFile onAction={onActionMock} />);
-  });
-});
