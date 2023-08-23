e373d5dca0e4b2f8c0ecfc80e98f6322d1056b71
Tao Huang <thuang@unc.edu>
2023-08-23 10:19:00 -0400

    Add scenario slider value to export csvv

--- src/Components/SimulationComponents/CsvMaker.js
+++ src/Components/SimulationComponents/CsvMaker.js
@@ -1,18 +1,36 @@
 import alertify from "alertifyjs";
 
-const csvMaker = (lambdaOutput) => {
+const csvMaker = (lambdaOutput, scenarioValues) => {
   try {
     // Empty array for storing the values
     let csvRows = [];
 
-    const headers = lambdaOutput[0].map((obj) => obj.Stage);
-    const headersWithWeek = ["Week", ...headers];
+    const headers = lambdaOutput[0]
+      .filter((obj) => obj.Stage !== "Movement Flow")
+      .map((obj) => obj.Stage);
+    const headersWithWeek = [
+      "Week",
+      ...headers,
+      "Leads",
+      "Opportunities",
+      "Progression",
+      "Closing",
+      "Win Rate",
+      "Market Dynamics",
+    ];
 
     csvRows.push(headersWithWeek.join(","));
 
     for (let i = 0; i < lambdaOutput.length; i++) {
-      const row = lambdaOutput[i].map((obj) => obj.values);
-      const rowWithWeek = [i + 1, ...row];
+      const row = lambdaOutput[i]
+        .filter((obj) => obj.Stage !== "Movement Flow")
+        .map((obj) => obj.values);
+      let rowWithWeek = [i + 1, ...row];
+
+      if (i === 0) {
+        rowWithWeek = [...rowWithWeek, scenarioValues];
+      }
+
       csvRows.push(rowWithWeek.join(","));
     }
 
