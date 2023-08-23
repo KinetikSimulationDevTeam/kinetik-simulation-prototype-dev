00a3a4f758645080fb93ec48f40dd286d3ad4a3a
Tao Huang <thuang@unc.edu>
2023-08-22 21:30:59 -0400

    Add parsing result to csv error handling

--- src/Components/SimulationComponents/DownloadCsv.js
+++ src/Components/SimulationComponents/DownloadCsv.js
@@ -1,21 +1,27 @@
+import alertify from "alertifyjs";
+
 const downloadCsv = function (data, filename) {
-  // Creating a Blob for having a csv file format
-  // and passing the data with type
-  const blob = new Blob([data], { type: "text/csv" });
+  try {
+    // Creating a Blob for having a csv file format
+    // and passing the data with type
+    const blob = new Blob([data], { type: "text/csv" });
 
-  // Creating an object for downloading url
-  const url = window.URL.createObjectURL(blob);
+    // Creating an object for downloading url
+    const url = window.URL.createObjectURL(blob);
 
-  // Creating a link element and configuring it
-  const link = document.createElement("a");
-  link.href = url;
-  link.download = filename; // Set the filename for the download
+    // Creating a link element and configuring it
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = filename; // Set the filename for the download
 
-  // Simulating a click on the link to trigger the download
-  link.click();
+    // Simulating a click on the link to trigger the download
+    link.click();
 
-  // Clean up by revoking the object URL
-  window.URL.revokeObjectURL(url);
+    // Clean up by revoking the object URL
+    window.URL.revokeObjectURL(url);
+  } catch (error) {
+    alertify.error("Error when downloading CSV");
+  }
 };
 
 export default downloadCsv;
