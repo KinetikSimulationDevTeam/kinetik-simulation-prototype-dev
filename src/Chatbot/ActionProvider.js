bfe4dbf8244fa8e43993d469eec9f19c3e85bbde
Tao Huang <thuang@unc.edu>
2023-12-09 23:58:48 -0500

    Add chatgpt to chatbot to handle other input

--- src/Chatbot/ActionProvider.js
+++ src/Chatbot/ActionProvider.js
@@ -1,18 +1,36 @@
-class ActionProvider {
-  constructor(
-    createChatBotMessage,
-    setStateFunc,
-    createClientMessage,
-    stateRef,
-    createCustomMessage,
-    ...rest
-  ) {
-    this.createChatBotMessage = createChatBotMessage;
-    this.setState = setStateFunc;
-    this.createClientMessage = createClientMessage;
-    this.stateRef = stateRef;
-    this.createCustomMessage = createCustomMessage;
-  }
-}
+import React from "react";
+import ChatGPTInvoke from "./ChatGPTInvoke";
+
+const ActionProvider = ({ createChatBotMessage, setState, children }) => {
+  const handleOtherInput = async (input) => {
+    let botMessage;
+    try {
+      const response = await ChatGPTInvoke(input);
+      botMessage = createChatBotMessage(response);
+    } catch (error) {
+      console.log(error);
+      botMessage = createChatBotMessage(
+        "I'm having some trouble, try again later."
+      );
+    }
+
+    setState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, botMessage],
+    }));
+  };
+
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          actions: {
+            handleOtherInput,
+          },
+        });
+      })}
+    </div>
+  );
+};
 
 export default ActionProvider;
--- src/Chatbot/MessageParser.js
+++ src/Chatbot/MessageParser.js
@@ -1,12 +1,24 @@
-class MessageParser {
-  constructor(actionProvider, state) {
-    this.actionProvider = actionProvider;
-    this.state = state;
-  }
+import React from "react";
 
-  parse(message) {
-    console.log(message);
-  }
-}
+const MessageParser = ({ children, actions }) => {
+  const parse = (message) => {
+    if (message.includes("tactic") || message.includes("strategy")) {
+      console.log("tactic");
+    } else {
+      actions.handleOtherInput(message);
+    }
+  };
+
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          parse: parse,
+          actions,
+        });
+      })}
+    </div>
+  );
+};
 
 export default MessageParser;
